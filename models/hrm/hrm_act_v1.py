fix: Attention should be causal in ACV1Block

models/hrm/hrm_act_v1.py
@@ -66,7 +66,7 @@ class HierarchicalReasoningModel_ACTV1Block(nn.Module):
             head_dim=config.hidden_size // config.num_heads,
             num_heads=config.num_heads,
             num_key_value_heads=config.num_heads,
-            causal=False
+            causal=True 
         )
         self.mlp = SwiGLU(
             hidden_size=config.hidden_size,